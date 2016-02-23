152fdda9357b71dc08eec650ae0e449e603b3ddd
aramis797 <justganesh12321@gmail.com>
2016-02-23 18:14:58 +0530

    initial project version

---
+++ Chapter_2/INSERTION_SORT.c
@@ -0,0 +1,24 @@
+/*
+A modified version of INSERTION SORT.
+It takes two arguments i.e array and its length
+*/
+
+#include <stdio.h>
+void Insertion(int A[],int n){
+	int key,temp,j;
+	for(int i=1;i<n;i++){
+		key=A[i];
+		j=i-1;
+		while(j >= 0 && A[j]>A[j+1]){
+			temp=A[j];A[j]=A[j+1];A[j+1]=temp;
+			j-=1;
+		}
+		A[j+1]=key;
+	}
+}
+int main(){
+	int A[5]={5,4,3,2,1};
+	for(int i=0;i<5;i++) printf("%d ",A[i]);printf("\n");
+	Insertion(A,5);
+	for(int i=0;i<5;i++) printf("%d ",A[i]);printf("\n");
+}
