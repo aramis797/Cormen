f077afb61c414cc30bda1fbe90c5d281f5bbd12e
aramis797 <justganesh12321@gmail.com>
2016-05-03 14:46:13 +0530

    Merge:java

---
+++ Chapter_2/MergeSort.java
@@ -0,0 +1,43 @@
+public class MergeSort{
+	public static void merge(int[] A,int p,int q,int r){
+		int n1 = q-p+1;
+		int n2 = r-q;
+		int[] L = new int[n1];
+		int[] R = new int[n2];
+		for(int i=0;i<n1;i++) L[i] = A[p+i];
+		for(int j=0;j<n2;j++) R[j] = A[q+j+1];
+		int li = 0,ri = 0;
+		for(int k=p;k<r+1;k++){
+			if(li < n1 && ri < n2){
+				if(L[li] <= R[ri]) A[k] = L[li++];
+				else A[k] = R[ri++];
+			}
+			else{
+				if(li == n1) A[k] = R[ri++];
+				else A[k] = L[li++];
+			}
+		}
+	}
+	public static void printArray(int A[],int len){
+		for(int i=0;i<len;i++){
+			System.out.print(A[i]);
+			System.out.print(", ");
+		}
+		System.out.println();
+	}
+	public static void mergeSort(int A[],int p,int r){
+		if(p<r){
+			int q = (p+r)/2;
+			mergeSort(A,p,q);
+			mergeSort(A,q+1,r);
+			merge(A,p,q,r);
+		}
+	}
+	public static void main(String args[]){
+		int[] A={5,4,3,2,1,6,8,7,9};
+		int l = 9;
+		printArray(A,l);
+		mergeSort(A,0,8);
+		printArray(A,l);
+	}
+}
